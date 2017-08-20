e31e10a604261d89f11c9912e369462caca685ea
Kyle Ruschel <dkr1985@icloud.com>
2017-08-20 17:52:41 -0400

    New Additions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-$(document).ready(function() {
       // Gets Link for Theme Song
       var audioElement = document.createElement("audio");
       audioElement.setAttribute("src", "./assets/music/gott.mp3");
@@ -11,4 +10,33 @@ $(document).ready(function() {
       $(".pause-button").on("click", function() {
         audioElement.pause();
     });
+
+var a1 = 'House Tarly';
+var a2 = 'House Tyrell';
+
+  function start () {
+  $('#main').html('<p> Which house does Samwell Tarly belong? </p>');
+  $('p').css({width: 800, height: 400});
+  $('p').css({"font-size": "60px", color: 'black', "font-family": 'papyrus', "background-color": "inherit", "text-align": "center"});
+}
+
+  function answers () {
+  $('#main').html('<button>' ({"class": 'btn btn-default btn-xs'}) + a1 + '</button>');
+  $('#btn-xs').css({"background-color": white});
+  
+}
+
+$('.start-button').on("click", function() {
+  $('#main').html('<img src="./assets/images/getready.gif" />');
+  $('img').css({width: 800, height: 400});
+  setTimeout(start, 5000);
+  setTimeout(answers, 10000);
 });
+
+
+
+
+
+
+
+
