fbebfdcb794620c8f94d970a1d1efd4a090431ce
Kyle Ruschel <dkr1985@icloud.com>
2017-08-22 01:41:16 -0400

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,97 @@
-//This function set everything to run once the webpage loads
 
-      var audioElement = document.createElement("audio"); //Here I am creating a new variable called "audio"
-      audioElement.setAttribute("src", "./assets/music/gott.mp3"); //Here I am setting that new variable to the source which holds the theme song
+var questions = [
+  {
+    question: "What is the Hound's Real Name?",
+    answer: "Sandor Clegane",
+    options: ["Gregor Clegane", "Beric Dondarrion", "Euron Greyjoy", "Sandor Clegane"],
+  },
+  {
+    question: "Ramsay Bolton Held Who Captive?",
+    answer: "Theon Greyjoy",
+    options: ["Sansa Stark", "Jon Snow", "Theon Greyjoy", "Tyrion Lannister"],
+  },
+  {
+    question: "What akjflksjf",
+    answer: "House Tarly",
+    options: ["House Tarly", "House Tyrell", "House Stark", "House Lannister"],
+  },
+  {
+    question: "What akjflksjf",
+    answer: "House Tarly",
+    options: ["House Tarly", "House Tyrell", "House Stark", "House Lannister"],
+  },
+  {
+    question: "What akjflksjf",
+    answer: "House Tarly",
+    options: ["House Tarly", "House Tyrell", "House Stark", "House Lannister"],
+  },
+];
 
-      
-      $(".theme-button").on("click", function() { //Once the user presses the Play Theme button, the song will play
-        audioElement.play();
-      });
 
-      $(".pause-button").on("click", function() { //Once the user presses the Pause Theme btton, the song will pause
-        audioElement.pause();
-    });
+var currentQ = 0;
+var correct = 0;
+var incorrect = 0;
 
-var option1 = 'House Tarly'; //An option for an answer the user will select
-var option2 = 'House Tyrell'; //An otpion for an answer the user will select
+change();
 
-$('.start-button').on("click", function() { 
-  $('#main').html('<img src="./assets/images/getready.gif" />'); //Once the user clicks the start button, this gif will play
-  $('img').css({width: 800, height: 400}); //This maintains the gif to the height and width of the previous image
-  setTimeout(start, 4000); //After 4 seconds this gif will change and take the value of function start
-  setTimeout(next, 8000); //After 8 seconds the start function will change and take the value of the function next
-  })
+var audioElement = document.createElement("audio"); //Here I am creating a new variable called "audio"
+audioElement.setAttribute("src", "./assets/music/gott.mp3"); //Here I am setting that new variable to the source which holds the theme song
 
-  $('.a1-btn').on("click", function () { 
-    $('#main').html('<img src="./assets/images/correct1.gif" />'); //If the user clicks the correct button, this gif will appear
+
+$(".theme-button").on("click", function() { //Once the user presses the Play Theme button, the song will play
+    audioElement.play();
 });
 
+$(".pause-button").on("click", function() { //Once the user presses the Pause Theme btton, the song will pause
+    audioElement.pause();
+});
 
-  
-  function start () {
-  $('#main').html('<p> Which house does Samwell Tarly belong? </p>');
-  $('p').css({width: 800, height: 400});
-  $('p').css({"font-size": "60px", color: 'black', "font-family": 'papyrus', "background-color": "inherit", "text-align": "center"});
-  
-}
-
-  function next () {
-  $('#main').html($('<button class="btn btn-default btn-xs a1-btn">' + option1 + '</button>')).append($('<button class="btn btn-default btn-xs a2-btn">' + option2 + '</button>'));
-  $('.a1-btn').css({"position": "absolute", "top": 100, "background-color": "white"});
-  $('.a2-btn').css({"position": "absolute", "top": 100, "left": 58, "background-color": "white"});
-  
-};
-
-  function next2 () {
-      $('#main').html('<img src="./assets/images/correct1.gif" />');
-      $('img').css({width: 800, height: 400});
+$(document).on("click", function() {
+  if((questions.answer === questions[currentQ].options[i])) {
+    $('.jumbotron').html('<img src="./assets/images/correct1.gif" />');
+    $('img').css({width: 800, height: 400});
+    $('.jumbotron').css({padding: 0});
   }
+  else {
+    $('.jumbotron').html('<img src="./assets/images/correct1.gif" />');
+    $('img').css({width: 800, height: 400});
+    $('.jumbotron').css({padding: 0});
+  }
+})
+
+$('#option1').on("click", function () {
+  $('.jumbotron').html('<img src="./assets/images/correct1.gif" />');
+  $('img').css({width: 800, height: 400});
+  $('.jumbotron').css({padding: 0});
+})
+
+$('#option2').on("click", function () {
+  $('.jumbotron').html('<img src="./assets/images/wrong1.gif" />');
+  $('img').css({width: 800, height: 400});
+  $('.jumbotron').css({padding: 0});
+})
+
+$('#option3').on("click", function () {
+  $('.jumbotron').html('<img src="./assets/images/wrong1.gif" />');
+  $('img').css({width: 800, height: 400});
+  $('.jumbotron').css({padding: 0});
+})
+
+$('#option4').on("click", function () {
+  $('.jumbotron').html('<img src="./assets/images/wrong1.gif" />');
+  $('img').css({width: 800, height: 400});
+  $('.jumbotron').css({padding: 0});
+})
 
 
+function change () {
+  var newDiv = ($('<div/>').addClass('text-center'));
+  var newH1 = ($('<h1/>').attr('id', 'title').text(questions[currentQ].question));
+  newDiv.append(newH1);
+  for( var i = 0; i < questions[currentQ].options.length; i++){
+    var option = ($('<input type="button" />').attr('id', 'option' + (i+1)).val(questions[currentQ].options[i]));
+    newDiv.append(option);
+  }
+  $('.jumbotron').html(newDiv);
 
-  
-
-
+}
